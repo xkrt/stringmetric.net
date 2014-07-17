0cd76264eefe05892b321dd0d893f55698ba4583
Pavel Martynov <mr.xkurt@gmail.com>
2014-07-17 15:56:55 +0400

    Levenstein distance with Wagner–Fischer algorithm

---
+++ src/StringMetric.Tests/LevensteinTests.fs
@@ -0,0 +1,20 @@
+﻿module StringMetric.Tests.LevensteinTests
+
+open NUnit.Framework
+open StringMetric
+
+[<Test>]
+let ``preconditions``() =
+    Levenstein.distance null null ==> (None, "null not allowed")
+    Levenstein.distance "" "" ==> (None, "empty strings not allowed")
+
+[<Test>]
+let ``distance``() =
+    Levenstein.distance "hello"
+                        "hello" ==> (Some 0, "equal strings")
+    Levenstein.distance "abc"
+                        "xyz" ==> (Some 3, "completly different strings")
+    Levenstein.distance "Kitten"
+                        "Sitten" ==> (Some 1, "one replacement")
+    Levenstein.distance "sittin"
+                        "SittinG" ==> (Some 2, "one replacement + one delete/insert")
---
+++ src/StringMetric/Levenstein.fs
@@ -0,0 +1,31 @@
+﻿module StringMetric.Levenstein
+
+open System
+
+// Wagner–Fischer algorithm
+
+let distance (str1: string) (str2: string): int option =
+    if String.IsNullOrEmpty str1 || String.IsNullOrEmpty str2
+    then None
+    else
+        let inline min3 one two three =
+            if one < two && one < three then one
+            elif two < three then two
+            else three
+
+        let m = str1.Length
+        let n = str2.Length
+        let d = Array2D.create (m + 1) (n + 1) 0
+
+        for i = 0 to m do d.[i, 0] <- i
+        for j = 0 to n do d.[0, j] <- j
+
+        for j = 1 to n do
+            for i = 1 to m do
+                if str1.[i-1] = str2.[j-1]
+                then d.[i, j] <- d.[i-1, j-1]
+                else
+                    d.[i, j] <- min3 (d.[i-1, j  ] + 1) // deletion
+                                     (d.[i  , j-1] + 1) // insertion
+                                     (d.[i-1, j-1] + 1) // substitution
+        Some d.[m,n]
